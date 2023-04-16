8acd6b8a47016afebe6e1fc172d1ed45178807ae
shfernan <shfernan@uwaterloo.ca>
2023-04-16 15:51:34 -0400

    Fixed issue with environment state test

--- src/sensor_fusion/test/test_env_state.cpp
+++ src/sensor_fusion/test/test_env_state.cpp
@@ -13,32 +13,36 @@ TEST(FindTargetObject, FindClosestObject) {
     // Add close and further objects to center lane track of trackedObjects
     env_state_test.add_object(close_center_object);
     env_state_test.add_object(far_center_object);
+
+    env_state_test.find_target_object();
     
-    
-    rclcpp::Time start = nh->get_clock()->now();
-    while (rclcpp::ok() && (nh->get_clock()->now() - start <= rclcpp::Duration::from_seconds(5.0)) ) {
-      rclcpp::Time time_now = nh->now();
+    '''
+    Block below should be in another test to check if objects cleared properly
+    '''
+  //   rclcpp::Time start = nh->get_clock()->now();
+  //   while (rclcpp::ok() && (nh->get_clock()->now() - start <= rclcpp::Duration::from_seconds(5.0)) ) {
+  //     rclcpp::Time time_now = nh->now();
 
-      // Stores the time difference since last time a message was received. If Kalman Filter
-      // has not sent a message in the past UPDATE_TOL, then reset everything to publish default values
-      double update_time_difference_centre = time_now.seconds() - env_state_test.last_msg_ros_timestamp_centre.seconds();
-      double update_time_difference_left = time_now.seconds() - env_state_test.last_msg_ros_timestamp_left.seconds();
-      double update_time_difference_right = time_now.seconds() - env_state_test.last_msg_ros_timestamp_right.seconds();
-      if (update_time_difference_centre > UPDATE_TOL) {
-        env_state_test.targetObjectsInLanes[0] = default_obj;
-      }
-      if (update_time_difference_left > UPDATE_TOL) {
-        env_state_test.targetObjectsInLanes[1] = default_obj;
-      }
-      if (update_time_difference_right > UPDATE_TOL) {
-        env_state_test.targetObjectsInLanes[2] = default_obj;
-      }
+  //     // Stores the time difference since last time a message was received. If Kalman Filter
+  //     // has not sent a message in the past UPDATE_TOL, then reset everything to publish default values
+  //     double update_time_difference_centre = time_now.seconds() - env_state_test.last_msg_ros_timestamp_centre.seconds();
+  //     double update_time_difference_left = time_now.seconds() - env_state_test.last_msg_ros_timestamp_left.seconds();
+  //     double update_time_difference_right = time_now.seconds() - env_state_test.last_msg_ros_timestamp_right.seconds();
+  //     if (update_time_difference_centre > UPDATE_TOL) {
+  //       env_state_test.targetObjectsInLanes[0] = default_obj;
+  //     }
+  //     if (update_time_difference_left > UPDATE_TOL) {
+  //       env_state_test.targetObjectsInLanes[1] = default_obj;
+  //     }
+  //     if (update_time_difference_right > UPDATE_TOL) {
+  //       env_state_test.targetObjectsInLanes[2] = default_obj;
+  //     }
 
-      env_state_test.find_target_object(); 
+  //     env_state_test.find_target_object(); 
 
-      rclcpp::spin_some(nh);
-      rclcpp::Rate(2000).sleep();
-  }
+  //     rclcpp::spin_some(nh);
+  //     rclcpp::Rate(2000).sleep();
+  // }
 
 
 
